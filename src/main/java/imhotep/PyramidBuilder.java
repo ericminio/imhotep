edfd8a1998af73ba80ce70dcc2985348cbd542f8
Eric Mignot <eric.mignot@gmail.com>
2014-03-19 07:03:02 -0400

    … youg Imhotep is not very wise yet

--- src/main/java/imhotep/PyramidBuilder.java
+++ src/main/java/imhotep/PyramidBuilder.java
@@ -2,19 +2,36 @@ package imhotep;
 
 import imhotep.harvesters.FileHarvester;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class PyramidBuilder {
 
+    private List<String> levels;
+    private FileHarvester fileHarvester;
+    private PyramidRenderer renderer;
+
     public void useHarvester(FileHarvester fileHarvester) {
-    }
-
-    public void useTools(PyramidRenderer renderer) {
-
+        this.fileHarvester = fileHarvester;
     }
 
     public void buildLevels(String... levels) {
+        this.levels = Arrays.asList( levels );
+    }
+
+    public void useTools(PyramidRenderer renderer) {
+        this.renderer = renderer;
     }
 
     public void go() {
+        List<Integer> sizes = new ArrayList<Integer>();
+        String fileContent;
+        while ((fileContent = fileHarvester.nextContent()) != null) {
+            int size = fileContent.split( "@Test" ).length - 1;
+            sizes.add( size );
+        }
+        renderer.render( levels, sizes );
     }
 
 }
